82e525624d66d4f5492d8dd2dfba672e17f77cba
GaelCANS <gael.levant@ca-normandie-seine.fr>
2018-02-08 16:48:13 +0100

    create migrations/controllers campaigns and markets

---
+++ database/migrations/2018_02_08_153009_create_campaigns_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->date('begin');
+            $table->date('end');
+            $table->boolean('cmm')->default(false);
+            $table->date('cmm_display');
+            $table->string('resource_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('campaigns');
+    }
+}
